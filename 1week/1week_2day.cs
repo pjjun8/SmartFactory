9ee481a468c38ea4d666665501936a833eb0354f
pjjun8 <pjjun898@gmail.com>
2024-06-26 14:31:42 +0900

    Update 1week_2day.cs

--- 1week/1week_2day.cs
+++ 1week/1week_2day.cs
@@ -30,3 +30,24 @@ namespace CToF
     }
 }
 ------------------------------------------------------------------
+using System;
+
+namespace CToF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //반지름을 입력 받으면 원넓이를 구해주는 프로그램을 작성하세요.
+            //단, 소수점 둘째자리까지만 출력하세요.
+            //단, 원주율은 Math 클래스를 사용하세요
+            double radius;
+            Console.WriteLine($"반지름을 입력하세요 : ");
+            radius = Int32.Parse(Console.ReadLine());
+
+            double area = Math.PI * radius * radius;
+
+            Console.WriteLine($"{area:F2}");
+        }
+    }
+}
