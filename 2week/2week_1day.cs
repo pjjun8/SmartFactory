8681b5d1ee373331f08876df9b8607c0f51ecc86
pjjun8 <pjjun898@gmail.com>
2024-07-01 10:54:35 +0900

    Update 2week_1day.cs

--- 2week/2week_1day.cs
+++ 2week/2week_1day.cs
@@ -1,1 +1,29 @@
-
+namespace Exam03
+{
+    class Sum
+    {
+        static int NumberAdd(int n)
+        {
+            int sum = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                sum += i;
+                Console.WriteLine(sum);
+            }
+            return sum; 
+        }
+        public int Get(int n)
+        { 
+            return NumberAdd(n);
+        }
+    }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum sum = new Sum();
+            Console.WriteLine(sum.Get(100));
+        }
+    }
+}
