0384ca456bf163f0b112676f0d7f24ac217b80be
pjjun8 <pjjun898@gmail.com>
2024-07-01 11:30:36 +0900

    Update 2week_1day.cs

--- 2week/2week_1day.cs
+++ 2week/2week_1day.cs
@@ -27,3 +27,43 @@ namespace Exam03
         }
     }
 }
+-----------------------------------------------------------------------------
+namespace Exam04
+{
+    //max를 구하는 메소드 GetMax(int[] arr) 를 만들어 주세요.
+    internal class Program
+    {//배열 요소값 중 가장 큰 값을 max로 대입 후 출력하라!!!
+        static int GetMax(int[] arr)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        //배열 요소값 중 가장 작은 값을 min로 대입 후 출력하라!!!
+        static int GetMin(int[] arr)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { -7, 5, 60, -33, 43 };
+
+            Console.WriteLine($"최대 값은: {GetMax(arr)}");
+            Console.WriteLine($"최소 값은: {GetMin(arr)}");
+        }
+    }
+}
