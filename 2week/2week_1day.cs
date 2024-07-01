1f05d4758599410353d7e44e086c6984f6e51191
pjjun8 <pjjun898@gmail.com>
2024-07-01 15:16:35 +0900

    Update 2week_1day.cs

--- 2week/2week_1day.cs
+++ 2week/2week_1day.cs
@@ -67,3 +67,45 @@ namespace Exam04
         }
     }
 }
+--------------------------------------------------------------------------
+namespace ScoreApp02
+{
+    internal class Program
+    {
+        //성적 입력 함수를 만들어 주세요. 3과목
+        static int[] InputThreeScore()
+        {
+            int[] score = new int[3];
+            for (int i = 0; i < score.Length; i++)
+            {
+                score[i] = int.Parse(Console.ReadLine());   
+            }
+            return score;
+        }
+        // 국어, 영어, 수학 성적을 합하는 코딩 
+        static int TotalScore(int[] score)
+        {
+            int totalscore = 0;
+            for (int i = 0; i < score.Length; i++)
+            {
+                totalscore += score[i];
+            }
+            return totalscore;
+        }
+        // 총점에서 평균값을 만드는 코딩
+        static double GetAvg(int totalScore, int[] score)
+        {
+            return totalScore / (double)score.Length;
+        }
+        static void Main(string[] args)
+        {
+            //1. 세 성적 입력받기
+            int[] score = InputThreeScore();
+            //2. 총점 구하기
+            Console.WriteLine($"total : {TotalScore(score)}");
+            //3. 평균 구하기
+            Console.WriteLine($"avg : {GetAvg(TotalScore(score), score):F2}");
+        }
+    }
+}
+-----------------------------------------------------------------------
