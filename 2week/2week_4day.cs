6ad53afd78dd761da38ecdf0f8fc328e837046ed
pjjun8 <pjjun898@gmail.com>
2024-07-04 16:36:03 +0900

    Update 2week_4day.cs

--- 2week/2week_4day.cs
+++ 2week/2week_4day.cs
@@ -162,3 +162,78 @@ namespace OOP07
     }
 }
 ---------------------------------------------------------------------
+namespace Quiz07
+{
+    /*완전수란 자신을 제외한 약수의 합이 자신이 되는 수를 완전수라고 합니다. 6은 완전수입니다.
+    6의 약수는 1 2 3 6 이 중 자신을 제외한 약수의 합 1 + 2 + 3 = 6 즉, 6은 완전수입니다.
+    1000 이하의 완전수를 입력 받아 yes, no로 표현해 주세요.*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int j = 0;
+            int[] sum = new int[1000];
+            int total = 0;
+            while (true)
+            {
+                int num = Int32.Parse(Console.ReadLine());
+                if (num <= 1000 && i < 1000)
+                {
+                    while (num > i)
+                    {
+                        if (num % i == 0)
+                        {
+                            sum[j] = i;
+                            total += sum[j];
+                            //Console.WriteLine(sum[j]);
+                            j++;
+                        }
+                        i++;
+                    }
+                    if (total == num || num ==1)
+                    {
+                        Console.WriteLine("yes");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("no");
+                        break;
+                    }
+                }
+                else { Console.WriteLine("1000이하 숫자 다시입력");}
+            }
+        }
+    }
+}
+-------------------------------------------------------------------
+namespace ConsoleApp5
+{   /*1 보다 큰 정수 N 가 1 과 N 자신 이외의 양의 약수를 가지지 않을 때의 N 을 소수라고 부른다.
+    이를테면, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31 등은 모두 소수이다.
+    4, 6, 16 등과 같이 소수가 아니면서 2 이상인 자연수를 합성수라고 정의하며,
+    1 은 소수도 아니고 합성수도 아닌 수이다.*/
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i <= 100; i++)
+            {
+                int count = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if(i % j == 0)
+                    {
+                        count++;
+                        break;
+                    }
+                }
+                if (count == 0)
+                {
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
+------------------------------------------------------------------
