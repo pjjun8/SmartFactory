9a08a506bad14c3894af534917bd8ed6f4420697
pjjun8 <pjjun898@gmail.com>
2024-07-05 11:18:32 +0900

    Update 2week_5day.cs

--- 2week/2week_5day.cs
+++ 2week/2week_5day.cs
@@ -20,4 +20,95 @@ namespace RandomApp01
         }
     }
 }
+---------------------------------------------------------------------------
+namespace RandomApp01
+{//로또번호 구하기 2단계 - 중복불가 내가한거
+    internal class Program
+    {   
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] rotto = new int[6];
+            int[] num = new int[6];
+            int bonus;
+            Console.Write($"로또번호: ");
+            for(int i = 0; i < 6; i++)
+            {
+                rotto[i] = random.Next(1, 50);
+                num[i] = rotto[i];
+            }
+            for(int i = 0;i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (rotto[i] == num[j] && i != j)
+                    {
+                        rotto[i] = random.Next(1, 50);
+                        rotto[j] = random.Next(1, 50);
+                        num[i] = rotto[i];
+                        num[j] = rotto[j];
+                        i = 0; j =0;
+                        break;
+                    }
+                }
+            
+            }
+          
+            bonus = random.Next(1, 50);
+            for (int i = 0; i < rotto.Length; i++)
+            {
+                if(bonus == rotto[i])
+                {
+                    bonus = random.Next(1, 50);
+                    i = 0;
+                }
+            }
+            for (int i = 0; i < rotto.Length; i++)
+            {
+                Console.Write($"{rotto[i]} ");
+            }
+            Console.Write($"\n보너스 번호: {bonus}");
+        }
+    }
+}
+---------------------------------------------------------------------
+namespace RandomApp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {//강사님이 한거
+            int[] numbers = new int[7];
+            Random random = new Random();
+
+            for (int i = 0; i < 7; i++)
+            {
+                numbers[i] = random.Next(1, 46);
+                //전수조사
+                for (int j = 0; j < i; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        i--;
+                        break;
+                    }
+                }
+            }
+
+            int bonusNumber = numbers[6];
+            int[] lottoNumbers = new int[6];
+            Array.Copy(numbers, 0, lottoNumbers, 0, 6);
+
+            Array.Sort(lottoNumbers);
+            foreach (int i in lottoNumbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"보너스 번호 {bonusNumber}");
+
+        }
+    }
+}
+------------------------------------------------------------------------------
 
