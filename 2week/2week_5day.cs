c287fd62e6094625470bfa3743ea1cd9b5a11502
pjjun8 <pjjun898@gmail.com>
2024-07-05 09:59:41 +0900

    Update 2week_5day.cs

--- 2week/2week_5day.cs
+++ 2week/2week_5day.cs
@@ -1,1 +1,23 @@
+namespace RandomApp01
+{
+    internal class Program
+    {   //국어, 영어, 수학 성적을 Rando을 사용하여 받고 평균만 출력해 보세요!
+        //
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] score = new int[3];
+            int total = 0;
+            double avg = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                score[i] = random.Next(0, 101);
+                Console.WriteLine(score[i]);
+                total += score[i];
+            }
+            avg = (double)total / score.Length;
+            Console.WriteLine(avg);
+        }
+    }
+}
 
