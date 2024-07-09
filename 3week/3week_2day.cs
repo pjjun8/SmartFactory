576a9a6c020bd3d9b1fc9062085d0b194e9fd13f
pjjun8 <pjjun898@gmail.com>
2024-07-09 09:49:45 +0900

    Update 3week_2day.cs

--- 3week/3week_2day.cs
+++ 3week/3week_2day.cs
@@ -37,3 +37,23 @@ namespace MethodTest
     }
 }
 --------------------------------------------------
+namespace Recursive01
+{
+    internal class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 1)
+                return n;
+            else
+                return n * Factorial(n - 1);
+        }
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine(Factorial(a));
+        }
+    }
+}
+
+-------------------------------------------------
