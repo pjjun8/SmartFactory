b2a658b68b1218cd00683dd9477d9c6dba069c22
pjjun8 <pjjun898@gmail.com>
2024-07-25 11:47:47 +0900

    Update 5week_4day.cs

--- 5week/5week_4day.cs
+++ 5week/5week_4day.cs
@@ -37,3 +37,50 @@ namespace ConsoleApp18
     }
 }
 ------------------------------------------------------------
+namespace FileTest01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "c:\\Temp\\abc.txt";
+            string content = "안녕!";
+
+            //쓰기
+            File.WriteAllText(path, content);
+            Console.WriteLine("파일 쓰기 성공");
+
+            //읽기
+            string path2 = "c:\\Temp\\ccc.txt";
+            try
+            {
+                string words = File.ReadAllText(path2);
+                Console.WriteLine(words);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("파일의 이름이 잘못되었습니다.");
+            }
+        }
+    }
+}
+------------------------------------------------------------
+namespace FileTest02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"c:\Temp\hello.txt";
+            string content = "안녕하세요 반갑습니다.";
+
+            using (StreamWriter writer = new StreamWriter(path))//using 있으면 Close() 안해줘도됨
+            {
+                writer.WriteLine(content);
+
+                //writer.Close(); // 이게 있어야 진짜서 글이 써짐
+            }
+            Console.WriteLine("현재 프로그램을 종료합니다.");
+        }
+    }
+}
