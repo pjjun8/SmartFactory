118b220286ec620e53865ec9aa421f8fae418f65
pjjun8 <pjjun898@gmail.com>
2024-07-25 17:45:18 +0900

    Update 5week_4day.cs

--- 5week/5week_4day.cs
+++ 5week/5week_4day.cs
@@ -84,3 +84,147 @@ namespace FileTest02
         }
     }
 }
+-------------------------------------------------------------
+namespace LingQuiz02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Char ch = 'A';
+            Char[] alpaabets = new char[26];
+
+            for(int i = 0; i <26; i++)
+            {
+                alpaabets[i] = ch++;
+            }
+
+            //Ling
+            var result = from c in alpaabets
+                         orderby c descending
+                         select c;
+            foreach( char e in result)
+            {
+                Console.WriteLine(e+ " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+------------------------------------------------------------
+namespace LinqExam03
+{
+    //p624
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+
+        public Person(string name, int age, string address)
+        {
+            Name = name;
+            Age = age;
+            Address = address;
+        }
+        public override string ToString()
+        {
+            return string.Format($"{Name}{Age}{Address}");
+        }
+    }
+    class MainLanguage
+    {
+        public string Name { get; set; }
+        public string Language { get; set; }
+
+        public MainLanguage(string name, string language)
+        {
+            Name = name;
+            Language = language;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>
+            {
+                new Person("Tom", 63, "Korea"),
+                new Person("Winnie", 40, "Tibet"),
+                new Person("Anders", 47, "Sudan"),
+                new Person("Hans", 25, "Tibet"),
+                new Person("Eureka", 32, "Sudan"),
+                new Person("Hawk", 15, "Korea")
+            };
+
+            List<MainLanguage> languages = new List<MainLanguage>
+            {
+                new MainLanguage("Anders", "Delphi"),
+                new MainLanguage("Anders", "C#"),
+                new MainLanguage("Tom", "Borland C++"),
+                new MainLanguage("Hans", "Visual C++"),
+                new MainLanguage("Winnie", "R")
+            };
+
+            //Ling
+            var all = from person in people
+                      select person;
+            //foreach (var item in all)
+            //{
+            //    Console.WriteLine($"{item.Name}, {item.Age}, {item.Address}");
+            //}
+            //Console.WriteLine(string.Join(Environment.NewLine, all));
+
+            var nameList1 = from person in people
+                           select person.Name;
+            foreach ( var item in nameList1 )
+            {
+                Console.WriteLine(item);
+            }
+
+            
+        }
+    }
+}
+------------------------------------------------------------------
+namespace LingStandardTest01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 3, 5, 6, 7, 9, 11, 13, 15, 20 };
+            int even = numbers.FirstOrDefault(n => n % 2 == 0);
+
+            if (even == 0)
+                Console.WriteLine("짝수가 없다.");
+            else
+                Console.WriteLine(even);
+        }
+    }
+}
+--------------------------------------------------------------------
+namespace LamdaExam04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action act1 = () => Console.WriteLine("Action()");
+            act1();
+
+            int result = 0;
+            Action<int> act2 = (x) => result = x * x;
+            act2(3);
+            Console.WriteLine(result);
+
+            Action<double, double> act3 = (x, y) =>
+            {
+                double pi = x / y;
+                Console.WriteLine(pi);
+            };
+            act3(22.0, 7.0);
+        }
+    }
+}
+
