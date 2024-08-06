1a775366d2ed594ad6000bf78bacec23a4f9e66d
pjjun8 <pjjun898@gmail.com>
2024-08-06 11:38:54 +0900

    Update 7week_2day.cs

--- 7week/7week_2day.cs
+++ 7week/7week_2day.cs
@@ -40,3 +40,50 @@ namespace TrackBarRGB
     }
 }
 --------------------------------------------------------------------
+namespace ProgresssBar01
+{
+    public partial class Form1 : Form
+    {
+        private int progressValue = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+
+            ProgressLabel.Text = "진행도 : 0%";
+
+            timer1.Interval = 100;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressValue++;
+
+            if(progressValue <= 100)
+            {
+                progressBar1.Value = progressValue;
+                ProgressLabel.Text = $"진행도 : {progressValue}%";
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("진행완료!");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressValue = 0;
+            progressBar1.Value = 0;
+            ProgressLabel.Text = "진행도 : 0%";
+            timer1.Start();
+        }
+    }
+}
+------------------------------------------------------------------
