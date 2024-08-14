d8310551b7df7513982728270ea569cbc2db3052
pjjun8 <pjjun898@gmail.com>
2024-08-14 14:39:05 +0900

    Update 8week_3day.cs

--- 8week/8week_3day.cs
+++ 8week/8week_3day.cs
@@ -64,3 +64,71 @@ namespace SimpleTCPClient
         }
     }
 }
+===================================================================================
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketTCPServer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. SetverSocket 만들기
+            IPAddress loacalAddr = IPAddress.Parse("127.0.0.1");
+            int port = 13000;
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            //2. Bind
+            serverSocket.Bind(new IPEndPoint(loacalAddr, port));
+            //3. Listen
+            serverSocket.Listen(1);
+            Console.WriteLine("연결을 기다리는 중...");
+            //4. Accept
+            Socket clientSocket = serverSocket.Accept();
+            Console.WriteLine("연결 성공!!!");
+            //5. Read/Write
+            string message = "안녕하세요. 클라이언트님!";
+            byte[] bytes = new byte[1024];
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            clientSocket.Send(data);
+            Console.WriteLine($"전송한 메시지 : {message}");
+            //6. Close
+            clientSocket.Shutdown(SocketShutdown.Both);
+            clientSocket.Close();
+        }
+    }
+}
+
+=====================================================================================
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SocketTCPClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Client소캣 만들기, 서버접속을 위한 소켓만들기
+            IPAddress serverAddr = IPAddress.Parse("127.0.0.1"); //친구서버 주소
+            int port = 13000;
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //2. Connect
+            clientSocket.Connect(new IPEndPoint(serverAddr, port));
+            Console.WriteLine("서버에 연결되었습니다.");
+            //3. Read,Write
+            byte[] bytes = new byte[1024];
+            int byteReceived = clientSocket.Receive(bytes);
+
+            //받은메시지 출력
+            string message = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine("서버로 부터 받은 내용 : " + message);
+            //4. Close
+            clientSocket.Close();
+        }
+    }
+}
+
