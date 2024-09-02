e45aef8f4b5054bbcd8e65d2726f796804b33cf1
pjjun8 <pjjun898@gmail.com>
2024-09-02 17:49:50 +0900

    Update 11week_1day.cs

--- 11week/11week_1day.cs
+++ 11week/11week_1day.cs
@@ -57,7 +57,7 @@ namespace ConsoleApp30
     }
 }
 ================================================================================
-// 그냥 콘솔 앱
+// 그냥 콘솔 앱 Add-Migration InitCRUD / update-database
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
@@ -119,3 +119,174 @@ namespace EF8_All
         }
     }
 }
+=========================================
+    using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF8_All
+{
+    public class Student
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        [MaxLength(30)]
+        public string Name { get; set; }
+
+        [MaxLength(30)]
+        public string HP { get; set; }
+    }
+
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = (local)\\SQLEXPRESS; " +
+                        "Database = MyDb; " +
+                        "Trusted_Connection = True;" +
+                        "Encrypt=False");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new StudentDbContext();
+
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
+
+            //데이터 삽입Create, Insert)
+            var st = new Student();
+            st.Name = "유재석";
+            st.HP = "010-1111-1111";
+
+            context.Students.Add(st);
+            context.SaveChanges();
+            Console.WriteLine("드감");
+
+            st = new Student();
+            st.Name = "강호동";
+            st.HP = "010-2222-2222";
+
+            context.Students.Add(st);
+            context.SaveChanges();
+            Console.WriteLine("드감");
+
+            //조회(select)
+            var list = context.Students.ToList();
+            foreach(var student in list)
+            {
+                Console.WriteLine(student.Name);
+            }
+            //수정(update)
+            var s1 = context.Students.First();
+            s1.Name = "박상원";
+            context.Dispose();
+        }
+    }
+}
+====================================================================
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication13.Models;
+
+namespace WebApplication13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(Student std)
+        {
+            if (ModelState.IsValid)
+            {
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+---------------------------------------------------
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication13.Models
+{
+    public class Student
+    {
+        [Display(Name = "이름을 입력하세요:")]
+        //[Required]
+        [Required(ErrorMessage = "이름란에 이름이 빠졌습니다.")]
+        //[MaxLength(15)]
+        [StringLength(15, MinimumLength = 2,
+            ErrorMessage = "이름은 두자 이상 작성이 가능합니다.")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email을 적어주세요..")]
+        //[EmailAddress]
+        [RegularExpression(@"^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$", ErrorMessage = "이메일 패턴이 맞지 않습니다.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "나이를 적어주세요..")]
+        [Range(20, 70, ErrorMessage = "20-70세까지 작성할 수 있습니다.")]
+        public int? Age { get; set; }
+
+        [Required(ErrorMessage = "패스워드를 적어주세요.")]
+        [RegularExpression(@"(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$", ErrorMessage = "영문자로 대소문자와 숫자조합으로 8자리이상")]
+        public string Password { get; set; }
+
+    }
+}
+-----------------------------------------------------------
+@model Student
+
+@{
+    ViewData["Title"] = "Home Page";
+}
+
+<div class="text-center">
+    <h1 class="display-4">Welcome</h1>
+</div>
+<div>
+    <form asp-controller="Home" asp-action="Index" method="post">
+        <!----------------------이름------------------------------------------>
+        <label asp-for="Name">이름 : </label>
+        <input asp-for="Name" class="form-control" placeholder="이름을 넣어 주세요"/>
+        <span asp-validation-for="Name" style="color:red"></span>
+        <!----------------------나이------------------------------------------>
+        <label asp-for="Age">나이 : </label>
+        <input asp-for="Age" class="form-control" placeholder="나이를 넣어 주세요" />
+        <span asp-validation-for="Age" style="color:red"></span>
+        <!----------------------패스워드------------------------------------------>
+        <label asp-for="Password">비밀번호 : </label>
+        <input asp-for="Password" class="form-control" placeholder="패스워드를 작성해주세요." />
+        <span asp-validation-for="Password" style="color:red"></span>
+        <!----------------------버튼------------------------------------------>
+        <input type="submit"value="OK" class="btn btn-outline-primary"
+    </form>
+</div>
