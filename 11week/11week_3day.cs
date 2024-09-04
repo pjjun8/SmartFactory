a0778f45b7c45696ae7b6a64b484aa98f026bcc9
pjjun8 <pjjun898@gmail.com>
2024-09-04 17:51:44 +0900

    Update 11week_3day.cs

--- 11week/11week_3day.cs
+++ 11week/11week_3day.cs
@@ -1,3 +1,18 @@
 //오전에는 어제했던 테이블 삽입, 삭제, 수정, 조회를 만드는 것을 해봤다. 오후에는 설문조사 작성 하는 것을 만들었는데 제작에 실패했다.
 //역시 디자인 쪽은 어렵다... 오후부터 본격적으로 2차 팀 프로젝트를 시작했다.
 //우리 4조는 광케이블을 주제로 MVC를 이용해서 공정정보, 제품 정보 등을 만들어 화면에 띄을 예정이다. 차후에 프로젝트 내용은 수정 될 수 있다!
+
+
+
+{
+  "Logging": {
+    "LogLevel": {
+      "Default": "Information",
+      "Microsoft.AspNetCore": "Warning"
+    }
+  },
+  "ConnectionStrings": {
+    "DefaultConnection": "Server=(local)\\SQLEXPRESS;Database=AndongDb;Trusted_Connection=True; Encrypt=False"
+  },
+  "AllowedHosts": "*"
+}
