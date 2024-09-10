6fcfd6ec1356d13bf2c6017ab1cc2c2a3e1a3dea
pjjun8 <pjjun898@gmail.com>
2024-09-10 16:34:12 +0900

    Update 12week_2day.cs

--- 12week/12week_2day.cs
+++ 12week/12week_2day.cs
@@ -1,1 +1,30 @@
-zz
+void setup( ) {
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop( ) {
+  char c;
+  if (Serial.available() > 0) {
+    c = Serial.read();
+    if (c == 'a') {  // 작은따옴표로 수정
+      digitalWrite(13, HIGH);  // 오타 수정
+    } else {
+      digitalWrite(13, LOW);
+    }
+  }
+}
+-------------------------
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int adcValue = analogRead(A0);
+  Serial.println(adcValue);
+  delay(100);
+
+}
