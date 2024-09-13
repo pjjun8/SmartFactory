12a4f141905135fc1c84e2d425e5986aec04052a
pjjun8 <pjjun898@gmail.com>
2024-09-13 10:14:17 +0900

    Update 12week_5day.cs

--- 12week/12week_5day.cs
+++ 12week/12week_5day.cs
@@ -1,1 +1,44 @@
+//LED하나 깜빡임
+#include "Timer.h"
+#define USER_LED	12
+Timer t;
+
+void setup() {
+  pinMode(USER_LED, OUTPUT);
+  t.oscillate(USER_LED, 1000, LOW);
+}
+
+void loop() {
+  t.update();
+}
+================================
+//LED 2개 교차 깜빡임
+#include "Timer.h"
+#define USER_LED  12
+#define BUILTIN_LED 13
+
+Timer t;
+
+void flip1(void) {
+  static int ledState = LOW;
+  digitalWrite(USER_LED, ledState );
+  ledState = !ledState;
+}
+
+void flip2(void) {
+  static int ledState = LOW;
+  digitalWrite(BUILTIN_LED, ledState );
+  ledState = !ledState;
+}
+
+void setup() {
+  pinMode(USER_LED, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  t.every(1000, flip1);
+  t.every(500, flip2);
+}
+
+void loop() {
+  t.update();
+}
 
