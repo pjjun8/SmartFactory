af5791efdf20df46a3805fe55ec4ee80d9bb9bb1
pjjun8 <pjjun898@gmail.com>
2024-09-26 17:39:06 +0900

    Update 14week_4day.cs

--- 14week/14week_4day.cs
+++ 14week/14week_4day.cs
@@ -2,6 +2,8 @@
 둘째 수업 부터 스팩에 참여해서 한혁이형 노션을 참조하여 오전에 못한 수업 내용(구글 코랩으로 파이썬 해보기)을 따라 잡았다.*/
 //코드 들은 노션에 캡쳐 해 놓았다.
 
+//오후에는 Influx DB on Raspberrypi 를 했다.
+
 // Phi_Publish_DHT11.ino
 #include <WiFi.h>
 #include <PubSubClient.h>
