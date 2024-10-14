0bd00f5c53d746496989c22c211c92485a9e81c9
pjjun8 <pjjun898@gmail.com>
2024-10-14 14:09:59 +0900

    Update 17week_1day.cs

--- 17week/17week_1day.cs
+++ 17week/17week_1day.cs
@@ -27,3 +27,29 @@ namespace OpenCVSharp002
         }
     }
 }
+=============================================================
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCVSharp004
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point pt1 = new Point(3, 4);
+            Mat m2 = new Mat(200, 300, MatType.CV_8U, new Scalar(200));
+
+            Cv2.ImShow("m2", m2);
+
+            Console.WriteLine($"p11({pt1.X}, {pt1.Y})");
+
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+        }
+    }
+}
