ef1875ff86ac93c8d04dd3990c3643322796dce3
pjjun8 <pjjun898@gmail.com>
2024-10-15 17:52:42 +0900

    Update 17week_2day.cs

--- 17week/17week_2day.cs
+++ 17week/17week_2day.cs
@@ -116,3 +116,106 @@ namespace Mat_Attr
         }
     }
 }
+==========================
+using OpenCvSharp;
+
+namespace OpenCvSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Scalar blue = new Scalar(255, 0, 0);
+            Scalar red = new Scalar(0, 0, 255);
+            Scalar green = new Scalar(0, 255, 0);
+            Scalar white = new Scalar(255, 255, 255);
+            Scalar yellow = new Scalar(0, 255, 255);
+
+            Mat image = new Mat(400, 600, MatType.CV_8UC3, white);
+            Point pt1 = new Point(50, 130);
+            Point pt2 = new Point(200, 300);
+            Point pt3 = new Point(300, 150);
+            Point pt4 = new Point(400, 50);
+            Rect rect = new Rect(pt3, new Size(200, 150));
+
+            //라인
+            Cv2.Line(image, pt1, pt2, red, 1, LineTypes.AntiAlias);
+            Cv2.Line(image, pt3, pt4, green, 2, LineTypes.AntiAlias);
+            Cv2.Line(image, pt3, pt4, green, 3, LineTypes.Link8, 1);
+
+            //사각형 그리기
+            Cv2.Rectangle(image, rect, blue, 2, LineTypes.AntiAlias);
+            Cv2.Rectangle(image, rect, blue, -1, LineTypes.AntiAlias, 1);
+            Cv2.Rectangle(image, pt1, pt2, red, 3);
+
+            //출력
+            Cv2.ImShow("image", image);
+
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
+        }
+    }
+}
+===================================
+using OpenCvSharp;
+
+namespace OpenCvSharp01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Scalar blue = new Scalar(255, 0, 0);
+            Scalar red = new Scalar(0, 0, 255);
+            Scalar green = new Scalar(0, 255, 0);
+            Scalar white = new Scalar(255, 255, 255);
+            Scalar yellow = new Scalar(0, 255, 255);
+            Scalar black = new Scalar(0, 0, 0);
+
+            Mat image = new Mat(600, 1000, MatType.CV_8UC3, white);
+            //Point pt1 = new Point(50, 100);
+            //Point pt2 = new Point(150, 100);
+            //Point pt3 = new Point(50, 150);
+            //Point pt4 = new Point(150, 150);
+
+            Point pt5 = new Point(400, 300); // 왼쪽 위 
+            Point pt6 = new Point(600, 500); // 오른쪽 아래
+            Point pt7 = new Point(600, 300); // 오른쪽 위
+            Point pt8 = new Point(400, 500); // 왼쪽 아래
+            Point pt9 = new Point(500, 200); // 삼각형 위
+            Point pt10 = new Point(500, 400); //원 중심좌표
+            Rect rect = new Rect(pt5, new Size(200, 200));
+
+            //라인
+            Cv2.Line(image, pt5, pt6, blue, 2, LineTypes.AntiAlias);
+            Cv2.Line(image, pt7, pt8, blue, 2, LineTypes.AntiAlias);
+            //Cv2.Line(image, pt3, pt4, green, 3, LineTypes.Link8, 1);
+
+            ////사각형 그리기
+            Cv2.Rectangle(image, rect, black, 2, LineTypes.AntiAlias);
+            //Cv2.Rectangle(image, rect, blue, -1, LineTypes.AntiAlias, 1);
+            //Cv2.Rectangle(image, pt1, pt2, red, 3);
+
+            //삼각형 그리기
+            //삼각형을 그리기 위한 점 배열
+            Point[] trianglePoints = { pt5, pt7, pt9 };
+
+            Cv2.FillPoly(image, new[] { trianglePoints }, red, LineTypes.AntiAlias);
+            //Cv2.Line(image, pt5, pt9, red, 2, LineTypes.AntiAlias);
+            //Cv2.Line(image, pt7, pt9, red, 2, LineTypes.AntiAlias);
+            //Cv2.Line(image, pt5, pt7, red, 2, LineTypes.AntiAlias);
+
+            //원 그리기
+            Cv2.Circle(image, pt10, 100, blue, -1);
+            //출력
+            Cv2.ImShow("image", image);
+
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
+        }
+    }
+}
