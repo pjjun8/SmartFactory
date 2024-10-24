31d3da99705ecd4c6703ac66320b953aa03137d7
pjjun8 <pjjun898@gmail.com>
2024-10-24 15:26:00 +0900

    Update 18week_4day.cs

--- 18week/18week_4day.cs
+++ 18week/18week_4day.cs
@@ -929,4 +929,176 @@ namespace CannyEgdeAlgorithm
         }
     }
 }
+====================================================
+using OpenCvSharp;
 
+namespace fileter_minMax
+{
+    internal class Program
+    {
+        static void MinMaxFilter(Mat img, out Mat dst, int size, int flag = 1)
+        {
+            dst = new Mat(img.Size(), MatType.CV_8U, Scalar.All(0));
+            Size msize = new Size(size, size);
+
+            Point h_m = new Point(msize.Width / 2, msize.Height / 2);   //h_m half of mask의 약자 mask의 중심점
+
+            for (int i = h_m.Y; i < img.Rows - h_m.Y; i++)
+            {
+                for (int j = h_m.X; j < img.Cols - h_m.X; j++)
+                {
+                    Point start = new Point(j, i) - h_m;
+                    Rect roi = new Rect(start, msize); // 마스크 영역 사각형
+                    Mat mask = new Mat(img, roi); // 마스크 영역 참조
+
+                    double minVal, maxVal;
+                    Cv2.MinMaxLoc(mask, out minVal, out maxVal); // 마스크 영역 최소, 최대값
+                    dst.Set(i, j, (byte)((flag != 0) ? maxVal : minVal));
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:/Temp/opencv/median_test1.jpg"; //시계그림 이름이 바뀌어 있네요 체크~!
+            Mat image = Cv2.ImRead(path, ImreadModes.Grayscale);
+
+            if (image.Empty())
+                throw new Exception("이미지를 불러올 수 없습니다.");
+
+            Mat minImg, maxImg;
+            MinMaxFilter(image, out minImg, 5, 0); // 5x5 마스크 최소값 필터링
+            MinMaxFilter(image, out maxImg, 5, 1); // 5x5 마스크 최대값 필터링
+
+            Mat cvMinImg = new Mat();
+            Mat cvMaxImg = new Mat();
+
+            // OpenCV의 Erode()와 Dilate() 함수를 사용한 최소값 및 최대값 필터링
+            Cv2.Erode(image, cvMinImg, new Mat(), iterations: 1); // 최소값 필터링
+            Cv2.Dilate(image, cvMaxImg, new Mat(), iterations: 1); // 최대값 필터링
+
+            // 결과 출력
+            Cv2.ImShow("image", image);
+            Cv2.ImShow("minFilter_img", minImg);
+            Cv2.ImShow("maxFilter_img", maxImg);
+            Cv2.ImShow("cvMinFilter_img", cvMinImg);
+            Cv2.ImShow("cvMaxFilter_img", cvMaxImg);
+            Cv2.WaitKey();
+        }
+    }
+}
+=================================================
+using OpenCvSharp;
+
+namespace AverageFilterTest
+{
+    internal class Program
+    {
+        static void AverageFilter(Mat img, out Mat dst, int size)
+        {
+            dst = new Mat(img.Size(), MatType.CV_8U, Scalar.All(0));
+
+            for (int i = 0; i < img.Rows; i++)
+            {
+                for (int j = 0; j < img.Cols; j++)
+                {
+                    Point pt1 = new Point(j - size / 2, i - size / 2);
+                    Point pt2 = pt1 + new Point(size, size);
+
+                    if (pt1.X < 0) pt1.X = 0;
+                    if (pt1.Y < 0) pt1.Y = 0;
+                    if (pt2.X > img.Cols) pt2.X = img.Cols;
+                    if (pt2.Y > img.Rows) pt2.Y = img.Rows;
+
+                    Rect maskRect = new Rect(pt1.X, pt1.Y, pt2.X - pt1.X, pt2.Y - pt1.Y);
+                    Mat mask = new Mat(img, maskRect);
+                    dst.Set(i, j, (byte)Cv2.Mean(mask)[0]);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:/Temp/opencv/avg_filter.jpg";
+            Mat image = Cv2.ImRead(path, ImreadModes.Grayscale);
+
+            if (image.Empty())
+                throw new Exception("이미지를 불러올 수 없습니다.");
+
+            Mat avgImg = new Mat();
+            Mat blurImg = new Mat();
+            Mat boxImg = new Mat();
+
+            AverageFilter(image, out avgImg, 5); // 사용자 정의 함수
+            Cv2.Blur(image, blurImg, new Size(5, 5)); // OpenCV 제공 블러 필터
+            Cv2.BoxFilter(image, boxImg, -1, new Size(5, 5)); // OpenCV 제공 박스 필터
+
+            Cv2.ImShow("원본", image);
+            Cv2.ImShow("avg_Filter 구현 알고리즘", avgImg);
+            Cv2.ImShow("OpenCV blur", blurImg);
+            Cv2.ImShow("OpenCV BoxFilter", boxImg);
+
+            Cv2.WaitKey();
+        }
+    }
+}
+===========================================
+using OpenCvSharp;
+
+namespace filter_median
+{
+    internal class Program
+    {
+        static void MedianFilter(Mat img, out Mat dst, int size)
+        {
+            dst = new Mat(img.Size(), MatType.CV_8U, Scalar.All(0));
+            Size msize = new Size(size, size);
+            Point h_m = new Point(msize.Width / 2, msize.Height / 2);
+
+            for (int i = h_m.Y; i < img.Rows - h_m.Y; i++)
+            {
+                for (int j = h_m.X; j < img.Cols - h_m.X; j++)
+                {
+                    Point start = new Point(j, i) - h_m;
+                    Rect roi = new Rect(start, msize);
+                    Mat mask = new Mat(img, roi);
+
+                    // 플랫하게 벡터로 변환하는 방식으로 수정 (직접 순환하면서 값을 넣음)
+                    byte[] maskArray = new byte[mask.Rows * mask.Cols];
+                    int index = 0;
+                    for (int y = 0; y < mask.Rows; y++)
+                    {
+                        for (int x = 0; x < mask.Cols; x++)
+                        {
+                            maskArray[index++] = mask.At<byte>(y, x);
+                        }
+                    }
+                    Array.Sort(maskArray);
+
+                    int mediIdx = maskArray.Length / 2; // 중간 위치
+                    dst.Set(i, j, maskArray[mediIdx]); // 중간값
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            string path = @"C:/Temp/opencv/median_test.jpg";
+            Mat image = Cv2.ImRead(path, ImreadModes.Color);
+
+            if (image.Empty())
+                throw new Exception("이미지를 불러올 수 없습니다.");
+
+            Mat gray = new Mat();
+            Mat medImg1 = new Mat();
+            Mat medImg2 = new Mat();
+            Cv2.CvtColor(image, gray, ColorConversionCodes.BGR2GRAY);
+
+            MedianFilter(gray, out medImg1, 5); // 사용자 정의 함수
+            Cv2.MedianBlur(gray, medImg2, 5); // OpenCV 제공 함수
+
+            Cv2.ImShow("gray", gray);
+            Cv2.ImShow("median-User", medImg1);
+            Cv2.ImShow("median-OpenCV", medImg2);
+
+            Cv2.WaitKey();
+        }
+    }
+}
